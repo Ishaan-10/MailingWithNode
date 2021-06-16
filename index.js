167dccb7515071a635ee3d78e216884ec28cceff
Ishaan-10 <b.ishaan10@gmail.com>
2021-06-16 21:12:08 +0530

    Added support for attachments , bcc and cc

--- index.js
+++ index.js
@@ -4,34 +4,36 @@ require('dotenv').config()
 
 async function main() {
 
-
-
   // create reusable transporter object using the default SMTP transport
   let transporter = nodemailer.createTransport({
     service: "Gmail",
     port: 587,
     secure: false, // true for 465, false for other ports
     auth: {
-      user: process.env.USER_EMAILID, // generated ethereal user
-      pass: process.env.USER_PASSWORD, // generated ethereal password
+      user: process.env.USER_EMAILID, 
+      pass: process.env.USER_PASSWORD,
     },
   });
 
   // send mail with defined transport object
   let info = await transporter.sendMail({
     from: '"Johnny 👻" <johnnychase1495@gmail.com>', // sender address
-    to: "ishaanbholayo@gmail.com, baz@example.com", // list of receivers
+    to: "ishaanbholayo@gmail.com,", // list of receivers
+    cc:"", //Carbon copy
+    bcc:"", // Blind carbon copy
+
     subject: "Mailing from Node.Js", // Subject line
     text: "Below should be html body", // plain text body
     html: "<b>Hello world (HTML)</b>", // html body
+    attachments: [{
+        filename:"test-image.gif", //Important to attach file extension
+        path:"./2471303.gif"
+    },{
+        href:"https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg"
+    }]
   });
 
   console.log("Message sent: %s", info.messageId);
-  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-
-  // Preview only available when sending through an Ethereal account
-  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
 }
 
 main().catch(console.error);
